b6531d871043558e51784f77b83b55512a1247bc
Joe <abhays@mail.smu.edu>
2022-12-13 15:49:23 -0800

    updates to Rshiny and commented RMD script

--- ui.R
+++ ui.R
@@ -7,6 +7,8 @@ fullset = read.csv('./Data/housing_fullset.csv')
 vars <- setdiff(names(iris), "Species")
 vars1 <- setdiff(names(fullset %>% select_if(is.numeric)), "Numerical")
 catvars <- setdiff(names(fullset %>% select_if(is.character)), "Categorical")
+bvars <- setdiff(names(fullset %>% select_if(is.numeric)), "Hist Selection")
+cvars <- setdiff(names(fullset %>% select_if(is.character)), "Categorical")
 
 
 pageWithSidebar(
@@ -14,9 +16,13 @@ pageWithSidebar(
   sidebarPanel(
     selectInput('xcol', 'Employee Statistic X-Axis', vars1),
     selectInput('ycol', 'Employee Statistic Y-Axis', vars1, selected = vars1[[2]]),
-    selectInput('zcol', "Categorical Seperation", catvars)
+    selectInput('zcol', "Categorical Seperation", catvars),
+    h3("See the distribution of other statistics!"),
+    selectInput('acol', 'Employee Histogram Stat', bvars),
+    selectInput('bcol', "Categorical Seperation", cvars),
   ),
   mainPanel(
-    plotOutput('plot1')
+    plotOutput('plot1'),
+    plotOutput('plot2')
   )
 )
